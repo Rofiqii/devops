e5317defb3b2b9f535ae2d06cc77ada1c9ff6a94
Rofiqi <e41222771@student.polije.ac.id>
2025-05-25 15:11:23 +0700

    update tabel

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
 
 class AdminSeeder extends Seeder
 {
@@ -13,14 +14,16 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('admin')->insert([
-            'username_admin' => 'admin',
-            'pw_admin' => Hash::make('123456789'),
-            'fullname_admin' => 'Administrator',
-            'pertanyaan' => 'Siapa nama ibu kandung Anda?',
-            'jawaban' => 'Admin',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Admin::updateOrCreate(
+            ['username_admin' => 'admin'], // The unique identifier to check
+            [
+                'pw_admin' => Hash::make('123456789'),
+                'fullname_admin' => 'Administrator',
+                'pertanyaan' => 'Apa makanan favoritmu?',
+                'jawaban' => 'nasi goreng',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
     }
 } 
